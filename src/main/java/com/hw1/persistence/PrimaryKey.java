15127201cd4fd80284eea0336c62c5508abe6bcd
Garrett Detwiler <gldetwiler@ucdavis.edu>
2026-02-14 20:01:58 -0800

    added definitions for annotations

--- src/main/java/com/hw1/persistence/PrimaryKey.java
+++ src/main/java/com/hw1/persistence/PrimaryKey.java
@@ -1,3 +1,12 @@
 package com.hw1.persistence;
 
 // Add the Persistable annotation definition here
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface PrimaryKey {}
--- src/main/java/com/hw1/persistence/RemoteLazyLoad.java
+++ src/main/java/com/hw1/persistence/RemoteLazyLoad.java
@@ -1,3 +1,11 @@
 package com.hw1.persistence;
 
 // Add the RemoteLazyLoad annotation definition here
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD) 
+public @interface RemoteLazyLoad {}

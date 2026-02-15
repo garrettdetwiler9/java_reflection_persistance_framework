7d7c859d52c0a160c9799bcbaf5fff334a13f93e
Garrett Detwiler <gldetwiler@ucdavis.edu>
2026-02-14 23:23:54 -0800

    defined loadRow(), made a proxy using Javassist for handling lazy loading, overriding invoke() for getters as well. Added disclaimers of AI use in README.md

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # AI usage disclosure
 
-[Fill this.]
+I used Gemini for figuring out which types of exceptions to throw/catch, 
+and for instantiating the actual proxy, as I was just unpracticed with Javassist
+
+I also don't know what to use the Hashset and Set libraries for
--- src/main/java/com/hw1/persistence/SQLiteDB.java
+++ src/main/java/com/hw1/persistence/SQLiteDB.java
@@ -1,6 +1,7 @@
 package com.hw1.persistence;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -90,25 +91,26 @@ public class SQLiteDB {
                 persistables.add(f);
             }
         }
+        int n = persistables.size();
 
         // make INSERT query
         StringBuilder sql = new StringBuilder("INSERT INTO ");
         sql.append(c.getSimpleName()).append(" ("); // " (" for col names
 
         // col names
-        for (int i = 0; i < persistables.size(); i++) {
+        for (int i = 0; i < n; i++) {
             Field f = persistables.get(i);
             sql.append(dbHelper.camelToSnakeCase(f.getName())); // make cases compatible between Java and SQLite
-            if (i < persistables.size() - 1) { // don't add for last item
+            if (i < n - 1) { // don't add for last item
                 sql.append(", "); // prep next 
             }
         }
         sql.append(") VALUES ("); // ")" ends col names, "VALUES (" sets up vals
 
         // temp names
-        for (int i = 0; i < persistables.size(); i++) {
+        for (int i = 0; i < n; i++) {
             sql.append("?"); // data will be inserted momentarily
-            if (i < persistables.size() - 1) { // don't add for last item
+            if (i < n - 1) { // don't add for last item
                 sql.append(", "); // prep next 
             }
         }
@@ -117,7 +119,7 @@ public class SQLiteDB {
         // perform INSERT query
         try (PreparedStatement ps = connection.prepareStatement(sql.toString())) {
             // set field vals
-            for (int i = 0; i < persistables.size(); i++) {
+            for (int i = 0; i < n; i++) {
                 Field f = persistables.get(i);
                 f.setAccessible(true);
                 Object val = f.get(obj);
@@ -147,11 +149,164 @@ public class SQLiteDB {
      * Check the handout for more details. 
      * 
      */
-    public <T> T loadRow(T obj) throws SQLException, IllegalAccessException {
+    // unchecked warning pmo
+    @SuppressWarnings("unchecked") 
+    // Gemini helped with the proxy instantiation, and its proposed solution required to add extra exception types in declaration
+    public <T> T loadRow(T obj) throws SQLException, IllegalAccessException, java.lang.NoSuchMethodException, InstantiationException, InvocationTargetException{
+        Class<?> c = obj.getClass();
+        Field[] fields = c.getDeclaredFields();
 
+        // locate primary key & its val
+        Field pkField = null;
+        Object pkVal = null;
+        for (Field f : fields) {
+            if (f.isAnnotationPresent(PrimaryKey.class)) {
+                pkField = f;
+                f.setAccessible(true);
+                pkVal = f.get(obj);
+                break; // only one primary key per class
+            }
+        }
 
-        // Return null if no row was found
-        return null;
+        // if we failed to find a primary key:
+        if (pkField == null || pkVal == null) {
+            System.err.println("Failed either to find primary key or its value");
+            System.exit(1);
+            return null; // Return null if no row was found
+        }
+
+        // store @Persistable fields in List
+        List<Field> persistables = new ArrayList<>();
+        for (Field f : fields) {
+            if (f.isAnnotationPresent(Persistable.class)) {
+                persistables.add(f);
+            }
+        }
+        int n = persistables.size();
+
+        // make SELECT query
+        // "SELECT *persistable field* FROM *class* WHERE *primary key field* = ?"
+        StringBuilder sql = new StringBuilder("SELECT ");
+        for (int i = 0; i < n; i++) {
+            Field f = persistables.get(i);
+            sql.append(dbHelper.camelToSnakeCase(f.getName()));
+            if (i < n - 1) {
+                sql.append(", ");
+            }
+        }
+        sql.append(" FROM ").append(c.getSimpleName());
+        sql.append(" WHERE ").append(dbHelper.camelToSnakeCase(pkField.getName())).append(" = ?");
+
+        // perform query
+        try (PreparedStatement ps = connection.prepareStatement(sql.toString())) {
+            // set pk val
+            if (pkField.getType() == String.class) {
+                ps.setString(1, (String) pkVal);
+            } else if (pkField.getType() == int.class || pkField.getType() == Integer.class) {
+                ps.setInt(1, (Integer) pkVal);
+            }
+
+            // resulting database set
+            try (ResultSet rs = ps.executeQuery()) {
+                // graceful exit if primary key can't be found
+                if (!rs.next()) {
+                    System.err.println("Could not find primary key");
+                    System.exit(1);
+                    return null;
+                }
+
+                // check for @RemoteLazyLoad
+                boolean isLazy = false;
+                List<String> lazyFields = new ArrayList<>();
+                for (Field f : persistables) {
+                    if (f.isAnnotationPresent(RemoteLazyLoad.class)) {
+                        isLazy = true;
+                        lazyFields.add(f.getName());
+                    }
+                }
+
+                // make new obj
+                T res;
+                try {
+                    res = (T) c.getDeclaredConstructor().newInstance();
+                } catch (ReflectiveOperationException e) {
+                    throw new RuntimeException("Could not create new instancee of " + c.getName(), e);
+                }
+
+                // populate fields
+                for (Field f : persistables) {
+                    f.setAccessible(true);
+                    String col = dbHelper.camelToSnakeCase(f.getName());
+                    
+                    if (f.getType() == String.class) {
+                        f.set(res, rs.getString(col));
+                    } else if (f.getType() == int.class || f.getType() == Integer.class) {
+                        f.set(res, rs.getInt(col));
+                    } else if (f.getType() == byte[].class) {
+                        f.set(res, rs.getBytes(col));
+                    }
+                }
+
+                // if no lazies, return curr state of res
+                if (!isLazy) {
+                    return res;
+                }
+
+                // make proxy for lazy loading with Javassist
+                ProxyFactory factory = new ProxyFactory();
+                factory.setSuperclass(c);
+
+                MethodHandler handler = new MethodHandler() {
+                    @Override
+                    public Object invoke(Object self, Method thisMethod, Method proceed, Object[] args) throws Throwable {
+                        String methodName = thisMethod.getName();
+
+                        // handle getters
+                        if (methodName.startsWith("get") && methodName.length() > 3) {
+                            // (getFieldName --> fieldName)
+                            String fieldName = methodName.substring(3, 4).toLowerCase() + methodName.substring(4);
+
+                            // lazy?
+                            if (lazyFields.contains(fieldName)) {
+                                // retreive field
+                                Field f = null;
+                                try {
+                                    f = c.getDeclaredField(fieldName);
+                                    f.setAccessible(true);
+                                    Object val = f.get(self);
+
+                                    // check for URL
+                                    if (val instanceof byte[]) {
+                                        byte[] bytes = (byte[]) val;
+                                        String possibleURL = new String(bytes);
+
+                                        if (possibleURL.startsWith("http")) {
+                                            // load stuff
+                                            byte[] loaded = dbHelper.fetchContentFromUrl(possibleURL);
+                                            return loaded;
+                                        }
+                                    }
+                                    return val;
+                                } catch (NoSuchFieldException e) {}
+                            }
+                        }
+
+                        // if not getter, just invoke
+                        return proceed.invoke(self, args);
+                    }
+                };
+
+                // create proxy obj with res obj
+                T resultProxy = (T) factory.create(new Class<?>[0], new Object[0], handler);
+                for (Field f : fields) {
+                    f.setAccessible(true);
+                    f.set(resultProxy, f.get(res));
+                }
+
+                return resultProxy;
+
+            }
+        }
     }
     
     
